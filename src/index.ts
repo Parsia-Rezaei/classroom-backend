64cd59b1ba140921a1eb4b247f4b5ddc86ca3fc8
Parsia-Rezaei <parsiarez@gmail.com>
2026-09-11 14:47:04 +0330

    initlized cors configs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import { subjectsRouter } from "./routes/subjects.route.js";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
 
 // JSON middleware
 app.use(express.json());
+app.use("/api/v1/subjects", subjectsRouter);
+app.use(
+  cors({
+    origin: process.env.FONTEND_URL,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  }),
+);
 
 // Root route
 app.get("/", (req, res) => {
